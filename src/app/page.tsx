ea238a7cc5506a2d4d86e77a4dda1c8a13fbbca7
Josepogers <zekreds12@gmail.com>
2024-10-22 19:38:16 -0300

    responsivar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 import Image from "next/image";
-import profileImage from "../../public/foto de rg.jpg"; // Atualizado
+import profileImage from "../../public/foto de rg.jpg";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen bg-gray-900 justify-center items-center">
-      <header className="flex fixed top-0 items-center justify-between w-full bg-white gap-4 h-16 px-4 md:gap-16">
+    <div className="flex flex-col min-h-screen bg-gray-900">
+      <header className="flex fixed top-0 items-center justify-between w-full bg-white h-16 px-4 z-10">
         <div>
           <Link href="/" className="font-bold text-black">
             José Francisco
           </Link>
         </div>
-        <div className="Links gap-2 md:gap-4 flex">
+        <div className="flex gap-2 md:gap-4">
           <Link href="/" className="font-bold text-black">
             Sobre mim
           </Link>
@@ -24,25 +24,36 @@ export default function Home() {
           </Link>
         </div>
       </header>
-      <main className="flex flex-grow w-full items-center justify-center bg-grey-500 px-4 md:px-16 gap-4 md:gap-96">
-        <div className="h-32">
-          <p className="text-lg md:text-2xl font-bold text-white text-center">
-            Olá! Me chamo José, tenho 17 anos e sou um apaixonado por
-            programação e tecnologia...
-          </p>
-        </div>
-        <div>
-          <Image
-            src={profileImage}
-            alt="profile"
-            width={2500}
-            height={200}
-            className="rounded-xl"
-          />
+
+      <main className="flex flex-grow flex-col items-center justify-center w-full px-4 md:px-16 pt-20 bg-gray-900">
+        <div className="flex flex-col md:flex-row items-center justify-between w-full gap-8 md:gap-16">
+          <div className="w-full md:w-1/2">
+            <p className="text-lg md:text-2xl font-bold text-white">
+              Olá! Sou José Francisco, tenho 17 anos e sou apaixonado por
+              tecnologia e programação. Estou sempre em busca de aprender novas
+              tecnologias e desenvolver minhas habilidades para se tornar um
+              profissional. Sou apaixonado por desenvolver soluções inovadoras e
+              explorando ferramentas como JavaScript. Além de programar, gosto
+              de praticar musculação para manter um equilíbrio entre corpo e
+              mente. Sou fã de animes, que me inspiram com suas histórias de
+              superação. Neste portfólio, você encontrará meus projetos e
+              desafios no mundo da tecnologia. Fique à vontade para explorar!
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <Image
+              src={profileImage}
+              alt="profile"
+              width={250} // Defina um valor menor para a largura
+              height={250} // Mantenha a altura para um aspecto mais quadrado
+              className="rounded-xl"
+            />
+          </div>
         </div>
       </main>
-      <footer className="bg-gray-700 text-center p-4 w-full fixed bottom-0">
-        <p>footer</p>
+
+      <footer className="bg-gray-700 text-center p-4 w-full fixed bottom-0 z-10">
+        <div>Github: @josepogers Linkedin: José Francisco Araújo</div>
       </footer>
     </div>
   );
